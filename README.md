4ca85bb03c313a294da069d685826bc5cf381c1e
rafael.lumang <lumang.rafael@gmail.com>
2023-08-27 16:49:19 -0700

    Added quote cycle every 5 seconds w/ and w/o click

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Random Quote Generator-2
  
+Random Quote Generator uses CSS and JS to show iconic quotes from the devil herself, Miranda Priestly, from "The Devil Wears Prada." 
+
+Click the "Show another quote" button to cycle through to the next quote. 
+Or let page run for 20 seconds to reveal a new quote. 
+
+(Project from teamtreehouse.com)
--- js/script.js
+++ js/script.js
@@ -1,89 +1,88 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
-
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
+// `quotes` array 
 const quotes = [
   {
     quote: "I had hope. Anyway, you ended up disappointing me more than any of the other silly girls",
     source: "Miranda Priestly",
     citation: "The Devil Wears Prada",
+    tag: "Movie",
     year: "2006",
   },
   {
     quote: "Find Me That Piece Of Paper I Had In My Hand Yesterday Morning.",
     source: "Miranda Priestly",
     citation: "The Devil Wears Prada",
+    tag: "Movie",
     year: "2006",
   },
   {
     quote: "There you are, Emily. How many times do I have to scream your name?",
     source: "Miranda Priestly",
     citation: "The Devil Wears Prada",
+    tag: "Movie",
     year: "2006",
   },
   {
     quote: "And this layout for the Winter Wonderland Spread. Not 'wonderful' yet",
     source: "Miranda Priestly",
     citation: "The Devil Wears Prada",
+    tag: "Movie",
     year: "2006",
   },
   {
     quote: "Is there some reason that my coffee isn’t here? Has she died or something?",
     source: "Miranda Priestly",
     citation: "The Devil Wears Prada",
+    tag: "Movie",
     year: "2006",
   },
   {
     quote: "By all means, move at a glacial pace. You know how that thrills me.",
     source: "Miranda Priestly",
     citation: "The Devil Wears Prada",
+    tag: "Movie",
     year: "2006",
   },
   {
     quote: "And R.S.V.P. Yes to Michael Kors’ party, I want the driver to drop me off at 9:30 and pick me up at 9:45 sharp.",
     source: "Miranda Priestly",
     citation: "The Devil Wears Prada",
+    tag: "Movie",
     year: "2006",
   },
   {
     quote: "Florals? For Spring? Groundbreaking.",
     source: "Miranda Priestly",
     citation: "The Devil Wears Prada",
+    tag: "Movie",
     year: "2006",
   },
   {
     quote: "Please bore someone else with your questions.",
     source: "Miranda Priestly",
     citation: "The Devil Wears Prada",
+    tag: "Movie",
     year: "2006",
   },
   {
     quote: "Oh, don't be ridiculous Andrea. Everybody wants this. Everybody wants to be us.",
     source: "Miranda Priestly",
     citation: "The Devil Wears Prada",
+    tag: "Movie",
     year: "2006",
   },
   {
     quote: "That's all.",
     source: "Miranda Priestly",
     citation: "The Devil Wears Prada",
+    tag: "Movie",
     year: "2006",
   }
 ];
 
-console.log(quotes);
+// Saving timeout ID for resetting after every click
+let timeout;
 
-/***
- * `getRandomQuote` function uses randomizer to select quote ID and pulls single quote from array
-***/
+// `getRandomQuote` function uses randomizer to select quote ID and pulls single quote from array
 
 function getRandomQuote(arr) {
   let id = Math.floor(Math.random() * arr.length);
@@ -91,32 +90,45 @@ function getRandomQuote(arr) {
   return quote;
 }
 
+// `randomRgbColor` returns a random color for the background
+function randomRgbColor() {
+  let r = Math.floor(Math.random() * 256); // Random between 0-255
+  let g = Math.floor(Math.random() * 256); // Random between 0-255
+  let b = Math.floor(Math.random() * 256); // Random between 0-255
+  let rgb = `rgb(${r}, ${g}, ${b})`;
+  return document.body.style.backgroundColor = rgb;
+};
 
 
-
-/***
- * `printQuote` function calls getRandomQuote on array of quotes and adds it to the HTML
-***/
-
+//`printQuote` function calls getRandomQuote on array of quotes and adds it to the HTML
 function printQuote() {
+  randomRgbColor();
   let printedQuote = getRandomQuote(quotes);
-  let HTML = `<p class="quote"> ${printedQuote.quote} </p>
-  <p class="source"> ${printedQuote.source}`;
+  let HTML = `<p class="quote"> ${printedQuote.quote}</p>
+  <p class="source"> ${printedQuote.source},`;
   if (printedQuote.citation) {
     HTML += `<span class="citation"> ${printedQuote.citation} </span>`
   };
-  if (printedQuote.source){
-    HTML += `  <span class="year"> ${printedQuote.year} </span>   `
+  if (printedQuote.year) {
+    HTML += `  <br>
+    <span class="year">(${printedQuote.tag}, ${printedQuote.year} )</span>   `
   };
   HTML += '</p>';
-  return document.getElementById('quote-box').innerHTML = HTML; 
+  return document.getElementById('quote-box').innerHTML = HTML;
+}
 
+// reset Timeout
+function setPrintQuoteTimeout() {
+  clearTimeout(timeout);
+  timeout = setTimeout(printQuote, 5000)
 }
 
 
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
+// Click event listener for the print quote button
+document.getElementById('load-quote').addEventListener("click", function () {
+  printQuote(); // change quote
+  setPrintQuoteTimeout(); // reset timer when clicked
+  }, false);
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+// Initialize timeout
+setPrintQuoteTimeout();
